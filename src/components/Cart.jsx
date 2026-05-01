5c951a91e3d979197ad8c1bc9865d27ecf0c5630
Jeremy Gitonga <jaybkaugi78@gmail.com>
2026-05-02 02:16:59 +0300

    fix Cart text

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,7 +3,7 @@ function Cart({ cartItems }) {
     <div>
       <h2>Shopping Cart</h2>
       {cartItems.map((item) => (
-        <p key={item.id}>{item.name} is in your cart.</p>
+        <p key={item.id}>{`${item.name} is in your cart.`}</p>
       ))}
     </div>
   );
